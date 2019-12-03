20a616b6276357291a5c25a53442acabda2a6312
Mark-Ball <markball111@gmail.com>
2019-12-04 07:28:12 +1100

    added prompt for choice to view, add, or remove a contact

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const inquirer = require('inquirer');
 
 console.log(chalk.blue("Hello World!"));
 
+let contactList = [];
+
 let addContactQuestions = [
     {
         type: "input",
@@ -17,13 +19,51 @@ let addContactQuestions = [
     }
 ]
 
+let removeContactQuestion = [
+    {
+        type: "list",
+        name: "contact",
+        message: "Which contact would you like to remove?",
+        choices: contactList
+    }
+]
+
 function addContact() {
-    let personalDetails = [];
     inquirer.prompt(addContactQuestions)
         .then(answers => {
-            personalDetails.push(answers);
-            console.log(personalDetails);
+            contactList.push(answers);
+            console.log(contactList);
         })
 }
 
-addContact();
+function removeContact() {
+    inquirer.prompt(removeContactQuestion)
+        .then(answer => {
+            console.log(answer);
+        })
+}
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "selection",
+        message: "What would you like to do?",
+        choices: [
+            "View contacts",
+            "Add a contact",
+            "Remove a contact"
+        ]
+    }
+])
+    .then(response => {
+        console.log(response)
+        if (response.selection === "View contacts") {
+            console.log(contactList);
+        } else if (response.selection === "Add a contact") {
+            addContact();
+        } else {
+            removeContact();
+        }
+    })
+
+// contactList.length === 0 ? console.log("There are no contacts to remove") : removeContact();
